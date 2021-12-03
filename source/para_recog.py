9c9a51563b9eaab62934ecd0c12ff387f89b7a2c
nadimhamdi <45047310+nadimhamdi@users.noreply.github.com>
2021-12-03 15:03:42 +0100

    Add files via upload

---
+++ source/para_recog.py
@@ -0,0 +1,20 @@
+from pytesseract import image_to_string 
+import os
+import cv2
+
+def para(ImageName_sans_terminisant,NomTableau):
+    in_file = os.path.join("Extraction/", "{}/{}".format(ImageName_sans_terminisant,NomTableau+'.png'))
+    try:
+        os.makedirs('Extraction/{}/Outs/{}/Image/'.format(ImageName_sans_terminisant, NomTableau))
+    except:
+        print('Extraction/{}/Outs/{}/Image'.format(ImageName_sans_terminisant, NomTableau)," existe deja")
+    img = cv2.imread(os.path.join(in_file))
+    image=img
+
+    cv2.imwrite('Extraction/{}/Outs/{}/Image/image.jpg'.format(ImageName_sans_terminisant,NomTableau), image)
+
+    save_path='Extraction/{}/Outs/{}/Image/resultat.txt'.format(ImageName_sans_terminisant,NomTableau)
+    text =  image_to_string(image)
+
+    with open(save_path, mode = 'w') as f:
+       f.write(text)
